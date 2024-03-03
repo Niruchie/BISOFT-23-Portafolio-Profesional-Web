bafd996e5e7b44f620b34d3aef7435152e398efc
Anthony Yanan Padilla Ureña <34629718+Niruchie@users.noreply.github.com>
2024-03-02 22:55:10 -0600

    feature(SmoothScroll,ESNext,Packages,GhPages)!: Updates to styles and some minor changes for portafolio

--- src/styles/Home.module.scss.d.ts
+++ src/styles/Home.module.scss.d.ts
@@ -2,6 +2,11 @@ declare const styles: {
   readonly "noclass": string;
   readonly "display": string;
   readonly "background-gradient": string;
+  readonly "sub": string;
+  readonly "movement": string;
+  readonly "move-skew": string;
+  readonly "move-anim": string;
+  readonly "move-anim2": string;
 };
 export = styles;
 
